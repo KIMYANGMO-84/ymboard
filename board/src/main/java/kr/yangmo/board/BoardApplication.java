3463e80392580d12c4184de95e5b340817b44990
KIM YANG MO <kim.yang.mo84@gmail.com>
2021-04-01 09:11:40 +0900

    Add files via upload

---
+++ board/src/main/java/kr/yangmo/board/BoardApplication.java
@@ -0,0 +1,23 @@
+package kr.yangmo.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@Bean
+	public HiddenHttpMethodFilter hiddenhttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
+
+}
